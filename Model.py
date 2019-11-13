ed0148a1390739fd5146d660f95234d7cc85936e
Youngyang <Youngyang@whu.edu.cn>
2019-11-14 00:59:59 +0800

    finished summary based on cosine-similarity of text and sentences.

--- Model.py
+++ Model.py
@@ -1,6 +1,7 @@
 import jieba
 import re
 import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 class ExtractiveSummary:
@@ -11,17 +12,18 @@ class ExtractiveSummary:
         self.min_len = min_len
         self.max_out = max_out
         self.dim = self.w2v_model.wv.vector_size
-        self.max_fre = max(self.frequency.values.tolist())
+        self.max_fre = max(self.frequency.values())
 
-    def tokenize(self, sentence): return re.findall(r'[\w|\d]+', sentence)
+    def _tokenize(self, sentence): return ''.join(re.findall(r'[\w|\d]+', sentence))
 
-    def cut(self, sentence): return ' '.join(jieba.cut(sentence))
+    def _cut(self, sentence): return ' '.join(jieba.cut(sentence))
 
     def sentence_embedding(self, sentence, smooth_alpha=1e-4):
         alpha = smooth_alpha
-        sentence = self.tokenize(sentence)
-        words = jieba.cut(sentence)
+        sentence = self._tokenize(sentence)
+        sentence = self._cut(sentence)
         sentence_vector = np.zeros(self.dim)
+        words = sentence.split()
         for word in words:
             if word in self.w2v_model.wv.vocab and word not in self.stop_words:
                 word_vec = self.w2v_model.wv[word]
@@ -30,8 +32,45 @@ class ExtractiveSummary:
         sentence_vector /= len(words)
         return sentence_vector
 
-    def text_embedding(self):
-        pass
+    def sentence_similarity(self, sent1, sent2):
+        sent1 = self.sentence_embedding(sent1)
+        sent2 = self.sentence_embedding(sent2)
+        cos = cosine_similarity(sent1.reshape(1, -1), sent2.reshape(1, -1))[0][0]
+        return cos
+
+    def split_sentences(self, text):
+        text = text.replace('\n', '')
+        text = text.replace('\r', '')
+        sentences = re.split('[,.，。?!？！]', text)
+        for i, sentence in enumerate(sentences):
+            if len(sentence) < self.min_len and i != len(sentences)-1:
+                sentences[i+1] = sentence + sentences[i+1]
+        split_sentence = [x for x in sentences if len(x) >= self.min_len]
+        return split_sentence
+
+    def get_correlation_rank(self, sentences):
+        text = ','.join(sentences)
+        cos = []
+        for sentence in sentences:
+            cos.append(self.sentence_similarity(text, sentence))
+        return [(sentences[i], i) for i in sorted(range(len(sentences)), key=lambda x: cos[x], reverse=True)]
+
+    def get_summary(self, text):
+        sentences = self.split_sentences(text)
+        ranked = self.get_correlation_rank(sentences)
+        summary = []
+        index = []
+        summary_len = 0
+        for sentence, i in ranked:
+            if summary_len > self.max_out:
+                return summary
+            index.append(i)
+            summary.append(sentence)
+            summary_len += len(sentence)
+        summary = sorted(summary, key=lambda x: index[x], reverse=False)
+        return ','.join(summary)
+
+
 
 
 
---
+++ test.py
@@ -0,0 +1,18 @@
+from Model import ExtractiveSummary
+from gensim.models import Word2Vec
+import pickle
+
+
+w2v = Word2Vec.load('w2v.bin')
+with open('frequence.pkl', 'rb') as f:
+    frequency = pickle.load(f)
+
+extract = ExtractiveSummary(w2v, frequency)
+
+text = '据江苏当地媒体报道，常州女子林某，30岁，未婚，与母亲王某同住。今年8月，林某与母亲发生争吵遭到殴' \
+           '打，报警后，王某被刑拘。在相关视频中，王某称，“正好菜架子上有个铁棍子，我就随手抄起来打了她一' \
+           '下。” 报道称，林某认为自己长期遭受母亲虐打，身心受到严重伤害，要母亲答应赔偿自己8万元损失费方可谅' \
+           '解。今日，上述相关视频在网络上传播，引起关注。新京报记者从常州市公安局天宁分局获悉，此前林某多次报警' \
+           '称遭到王某殴打，因情节轻微，未达到立案标准，警方选择进行民事调解。此次林某鉴定为轻伤二级，警方立案调' \
+           '查并对王某依法刑拘。目前，王某已被取保候审。至于母女双方调解的具体过程及赔偿金额等，警方表示不便透露。'
+print(extract.get_summary(text))
